4228e367a742afccabb9fa93a1e714bf717fdb02
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2025-03-11 13:24:13 +0100

    first commit

---
+++ OOP3 Back Jack/Classes/Card.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3_Back_Jack
+{
+    public enum Suits
+    {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    public enum FaceValues
+    {
+        ACE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    }
+    public class Card
+    {
+        Suits suit;
+        FaceValues faceValue;
+        int value;
+        Image img;
+
+        public Suits Suit { 
+            get { 
+                return suit; 
+                } 
+        }
+        public int Value
+        {
+            get { return value; }
+            set
+            {
+                if (faceValue == FaceValues.ACE)
+                {
+                    this.value = value;
+                }
+            }
+        }
+
+        public Card(Suits suit, FaceValues faceValue)
+        {
+            this.suit = suit;
+            this.value = value;
+            switch (faceValue)
+            {
+                case FaceValues.ACE:
+                    this.value = 11;
+                    break;
+                case FaceValues.TEN:
+                case FaceValues.JACK:
+                case FaceValues.QUEEN:
+                case FaceValues.KING:
+                    this.value = 10;
+                    break;
+                default:
+                    this.value = (int)faceValue;
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.value + "of" + this.suit;
+        }
+    }
+}
--- OOP3 Back Jack/Form1.cs
+++ OOP3 Back Jack/Form1.cs
@@ -1,10 +1,23 @@
+using OOP3_Back_Jack.Classes;
+
 namespace OOP3_Back_Jack
 {
     public partial class Form1 : Form
     {
+        Card twoOfHearts = new Card(Suits.HEARTS, FaceValues.TWO);
         public Form1()
         {
             InitializeComponent();
+            Console.WriteLine(twoOfHearts.ToString());
+            Console.WriteLine(twoOfHearts.Value);
+            twoOfHearts.Value = 3;
+            Console.WriteLine(twoOfHearts.Value);
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World!");
         }
     }
 }
