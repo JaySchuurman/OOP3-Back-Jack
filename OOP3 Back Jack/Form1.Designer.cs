baf9454ff7de61c0da8f18ab3195b4f8ca073fed
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2025-04-04 13:41:35 +0200

    Made some button changes

--- OOP3 Back Jack/Form1.Designer.cs
+++ OOP3 Back Jack/Form1.Designer.cs
@@ -30,33 +30,45 @@
         {
             button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Location = new Point(12, 12);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(94, 92);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Shuffle Deck";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(112, 12);
+            button2.Location = new Point(594, 12);
             button2.Name = "button2";
-            button2.Size = new Size(94, 29);
+            button2.Size = new Size(94, 92);
             button2.TabIndex = 1;
-            button2.Text = "button2";
+            button2.Text = "Stand";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(694, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 92);
+            button3.TabIndex = 2;
+            button3.Text = "Hit";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
@@ -68,5 +80,6 @@
 
         private Button button1;
         private Button button2;
+        private Button button3;
     }
 }
--- OOP3 Back Jack/Form1.cs
+++ OOP3 Back Jack/Form1.cs
@@ -27,5 +27,10 @@ namespace OOP3_Back_Jack
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
